648b8e38902022110f8aed88dc14a8bd18e9eff4
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-02-23 00:13:00 -0800

    initial commit

---
+++ MvcProjectManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcProjectManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
