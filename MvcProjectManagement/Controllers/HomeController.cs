d1267af24d2dde30d76a1a4234c482d26d82c636
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2021-03-28 01:44:02 -0700

    intermediate

--- MvcProjectManagement/Controllers/HomeController.cs
+++ MvcProjectManagement/Controllers/HomeController.cs
@@ -11,27 +11,10 @@ namespace MvcProjectManagement.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [Route("Home/Index")]
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- MvcProjectManagement/Startup.cs
+++ MvcProjectManagement/Startup.cs
@@ -28,7 +28,7 @@ namespace MvcProjectManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
